019ab9e5030926c2967871df14c08a2b2950b3e8
RhuanThales <rhuantst@gmail.com>
2019-03-31 11:01:36 -0300

    Inicialização do Firebase e Sistema de Login e de Cadastro de Usuário
    Inicialização do firebase e Sistema de Login e de Cadastro de novo Usuário usando e-mail e senha.
    Feito por Rhuan Thales.

---
+++ public/js/app-firebase.js
@@ -0,0 +1,11 @@
+// Inicializando o Firebase (esses dados são obtidos no console do projeto do firebase)
+var config = {
+  apiKey: "AIzaSyDu72_e-MMBdf9QS-GXYzN53tArXNECW9M",
+  authDomain: "community-report-si.firebaseapp.com",
+  databaseURL: "https://community-report-si.firebaseio.com",
+  projectId: "community-report-si",
+  storageBucket: "community-report-si.appspot.com",
+  messagingSenderId: "629365998301"
+};
+
+firebase.initializeApp(config);
---
+++ public/js/meu-script.js
@@ -0,0 +1,56 @@
+//Autenticação do Firebase
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // Há um usuário logado
+  } else {
+    // Não há usuário logado
+  }
+});
+
+//Função que permite fazer o login de usuário com e-mail e senha já cadastrados
+function login(){
+	var userEmail = document.getElementById('inputEmail').value;
+	var userSenha = document.getElementById('inputSenha').value;
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userSenha).then(function(){
+		//Em caso de sucesso redireciona para a página inicial (index.html)
+		window.alert("Login com Sucesso!!!");
+		window.open("index.html", "_self");
+	}).catch(function(error) {
+	  // Em caso de algum erro exibir uma mensagem com o erro que ocorreu.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  window.alert("Error: " + errorMessage);
+	});
+}
+
+//Função que cria um novo usuário com email e senha
+function sigin(){
+	var userEmail = document.getElementById('inputEmail').value;
+	var userSenha = document.getElementById('inputSenha').value;
+
+	firebase.auth().createUserWithEmailAndPassword(userEmail, userSenha).then(function(){
+		//Em cado de sucesso redireciona para a página de login
+		window.alert("Cadastrado com Sucesso!!!");
+		window.open("login-usuario.html", "_self");
+	}).catch(function(error) {
+	  // Em caso de algum erro exibir uma mensagem com o erro que ocorreu.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  window.alert("Error: " + errorMessage);
+	});
+}
+
+//Função que permite que o usuário que esteja logado possa sair de sua conta
+function logout(){
+	firebase.auth().signOut().then(function() {
+		// Aqui o usuário foi deslogado com sucesso e portanto o usuário é redirecionado á página de login
+		window.alert("Logout com Sucesso!!!");
+		window.open("login-usuario.html", "_self");
+	}).catch(function(error) {
+	  // Em caso de algum erro exibir uma mensagem com o erro que ocorreu.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  window.alert("Error: " + errorMessage);
+	});
+}
