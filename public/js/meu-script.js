fd13a240b797cb8b979dcec86cc1f550ecee5566
Rhuan Thales <rhuantst@gmail.com>
2019-05-29 10:00:21 -0300

    Adaptações para remoção de erros no console e implementação do service worker

--- public/js/meu-script.js
+++ public/js/meu-script.js
@@ -397,7 +397,13 @@ function gestorQuery(prefeitura,setor){
 					var col2 = document.createElement("div");
 					$(col2).addClass("col-md-6");
 					$(col2).addClass("ocorrDiv");
-					
+
+					var cardDiv = document.createElement("div");
+					$(cardDiv).addClass("card"); //Add aqui a imagem a a outra div
+
+					var cardBody = document.createElement("div");
+					$(cardBody).addClass("card-body"); //Add aqui um h5 (card-title) e um p (card-text) e os botoes
+
 					//Criando div's para que possam ser colocados alguns itens dentro delas			
 					var ocorrDiv1 = document.createElement("div");
 					$(ocorrDiv1).addClass("ocorrDiv1");
@@ -447,9 +453,11 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
+
 						});
 						//
 						var divBtn = document.createElement("div");
@@ -481,6 +489,9 @@ function gestorQuery(prefeitura,setor){
 						$(ocorrEnd).addClass("ocorrInfo");
 						$(ocorrEnd).html(currentObject.regRua);
 
+						var idTeste = currentObject.idOcorr;
+						console.log(idTeste);
+
 						//Criação dos botões para o gerenciamento das ocorrências
 						var ocorrActionBtn1 = document.createElement("button");
 						ocorrActionBtn1.innerHTML = 'Resolvido';
@@ -496,6 +507,9 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						//ocorrActionBtn3.id = currentObject.idOcorr;
+						//ocorrActionBtn3.setAttribute('btnid', currentObject.idOcorr);
+						ocorrActionBtn3.value = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -545,6 +559,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -594,6 +609,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -643,6 +659,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -692,6 +709,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -741,6 +759,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -790,6 +809,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -839,6 +859,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -888,6 +909,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -937,6 +959,7 @@ function gestorQuery(prefeitura,setor){
 						});
 						var ocorrActionBtn3 = document.createElement("button");
 						ocorrActionBtn3.innerHTML = 'Não resolvido';
+						ocorrActionBtn3.id = currentObject.idOcorr;
 						$(ocorrActionBtn3).addClass("btn btn-danger");
 						$(ocorrActionBtn3).on("click", function(event){
 							window.alert("Marcado como não resolvido!");
@@ -959,362 +982,7 @@ function gestorQuery(prefeitura,setor){
 	});
 }
 
-//Função para redirecionar a pagina de registro de ocorrencias
-$(document).ready(function(){
-	$("#occorDiv").show();
-	$("#regDiv").hide();
-	$("#menuDiv").show();
-	$("#returnArrow").show();
-	$("#backArrow").hide();
-	$("#btnRegistrarOcorr").hide();
-	$("#btnRegOcorrencia").show();
-	$("#sectionReOcorr").hide();
-	//
-	$("#sairBtnGestor").show();
-	$("#retornarBtnGestor").hide();
-});
-
-var btnRegAnimal = document.getElementById('btnRegAnimal');
-var btnRegArvore = document.getElementById('btnRegArvore');
-var btnRegBuraco = document.getElementById('btnRegBuraco');
-var btnRegInseto = document.getElementById('btnRegInseto');
-var btnRegLampada = document.getElementById('btnRegLampada');
-var btnRegLimpeza = document.getElementById('btnRegLimpeza');
-var btnRegLote = document.getElementById('btnRegLote');
-var btnRegBarulho = document.getElementById('btnRegBarulho');
-var btnRegSinal = document.getElementById('btnRegSinal');
-var btnRegAgua = document.getElementById('btnRegAgua');
-var btnRegOutros = document.getElementById('btnRegOutros');
-var btnBackArrow = document.getElementById('backArrow');
-
-btnRegAnimal.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").show();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegArvore.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").show();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegBuraco.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").show();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegInseto.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").show();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegLampada.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").show();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegLimpeza.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").show();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegLote.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").show();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegBarulho.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").show();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegSinal.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").show();
-	$("#selectAgua").hide();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegAgua.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").show();
-	$("#descSimples").show();
-	$("#descCompleta").hide();
-	$("#descLabelC").hide();
-});
-btnRegOutros.addEventListener('click', function(){
-	$("#ocorrDiv").hide();
-	$("#regDiv").show();
-	$("#menuDiv").hide();
-	$("#returnArrow").hide();
-	$("#backArrow").show();
-	$("#probLabel").hide();
-	$("#selectAnimal").hide();
-	$("#selectArvore").hide();
-	$("#selectBuraco").hide();
-	$("#selectLampada").hide();
-	$("#selectLimpeza").hide();
-	$("#selectLote").hide();
-	$("#selectInseto").hide();
-	$("#selectBarulho").hide();
-	$("#selectSinal").hide();
-	$("#selectAgua").hide();
-	$("#descSimples").hide();
-	$("#descCompleta").show();
-	$("#descLabelS").hide();
-});
-
-//Função para salvar os dados da ocorrência no banco do firebase
-var selectedFile;
-
-document.getElementById("upload").addEventListener('change', handleFileSelect, false);
-
-function handleFileSelect(event) {
-	selectedFile = event.target.files[0];
-	$("#btnRegistrarOcorr").show();
-	$("#btnRegOcorrencia").hide();
-};
-
-//Função para salvar os dados da ocorrências com a url da imagem selecionada e salvar a imagem no storege do firebase na pasta apropriada
-function regOcorrencia() {
-	//Criando uma referencia ao root
-	var filename = selectedFile.name;
-	var storageRef = firebase.storage().ref('/fotos-ocorrencias/' + filename);
-	var uploadTask = storageRef.put(selectedFile);
-
-	uploadTask.on('state_changed', function(snapshot){
-
-	},function(error){
-		//Em caso de der algum erro ao enviar os dados
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		window.alert("Error: " + errorMessage);
-	
-	},function(){
-		//Em caso de sucesso ao enviar os dados
-		var postKey = firebase.database().ref('Ocorrencias-Registradas/').push().key;
-		var downloadURL = uploadTask.snapshot.downloadURL;
-		var updates = {};
-		var postData = {
-			imagemURL: downloadURL,
-			regProblema1: $("#selectAnimal").val(),
-			regProblema2: $("#selectArvore").val(),
-			regProblema3: $("#selectBuraco").val(),
-			regProblema4: $("#selectLampada").val(),
-			regProblema5: $("#selectLimpeza").val(),
-			regProblema6: $("#selectLote").val(),
-			regProblema7: $("#selectInseto").val(),
-			regProblema8: $("#selectBarulho").val(),
-			regProblema9: $("#selectSinal").val(),
-			regProblema0: $("#selectAgua").val(),
-			regEmail: $("#regEmail").val(),
-			regRua: $("#regRua").val(),
-			regNumero: $("#regNum").val(),
-			regBairro: $("#regBairro").val(),
-			regCidade: $("#regCidade").val(),
-			regEstado: $("#regEstado").val(),
-			regDescricaoS: $("#descSimples").val(),
-			regDescricaoC: $("#descCompleta").val(),
-			regSetor: "Ouvidoria",
-			regStatus: "Não Resolvido"
-		};
-		updates['/Ocorrencias-Registradas/' + postKey] = postData;
-		firebase.database().ref().update(updates);
-		//Apos salvar os dados o formulario de preenchimento é ocultado
-		window.alert("Sucesso no registro da ocorrencia!!!");
-		window.open("ocorrencias.html", "_self");
-	});
-}
-
-//Função que salva a ocorrência registrada, sem colocar foto, no banco de dados
-function regOcorr(){
-	var postKey = firebase.database().ref('Ocorrencias-Registradas/').push().key;
-	var updates = {};
-	var postData = {
-		regProblema1: $("#selectAnimal").val(),
-		regProblema2: $("#selectArvore").val(),
-		regProblema3: $("#selectBuraco").val(),
-		regProblema4: $("#selectLampada").val(),
-		regProblema5: $("#selectLimpeza").val(),
-		regProblema6: $("#selectLote").val(),
-		regProblema7: $("#selectInseto").val(),
-		regProblema8: $("#selectBarulho").val(),
-		regProblema9: $("#selectSinal").val(),
-		regProblema0: $("#selectAgua").val(),
-		regEmail: $("#regEmail").val(),
-		regRua: $("#regRua").val(),
-		regNumero: $("#regNum").val(),
-		regBairro: $("#regBairro").val(),
-		regCidade: $("#regCidade").val(),
-		regEstado: $("#regEstado").val(),
-		regDescricaoS: $("#descSimples").val(),
-		regDescricaoC: $("#descCompleta").val(),
-		regSetor: "Ouvidoria",
-		regStatus: "Não Resolvido"
-	};
-	updates['/Ocorrencias-Registradas/' + postKey] = postData;
-	firebase.database().ref().update(updates);
-	//Apos salvar os dados o formulario de preenchimento é ocultado
-	window.alert("Sucesso no registro da ocorrencia!!!");
-	window.open("ocorrencias.html", "_self");
-}
-
 //Funções para se refazer as ocorrências que ainda não foram solucionadas pelo setor responsavel
-function reOcorr(){
-	$("#ocorrencias").hide();
-	$("#sectionReOcorr").show();
-}
-
-function cancelarReOcorr(){
-	$("#ocorrencias").show();
-	$("#sectionReOcorr").hide();
-}
-
 function fazerReOcorr(){
 	var postKey = firebase.database().ref('Ocorrencias-Refeitas/').push().key;
 	var updates = {};
@@ -1324,11 +992,15 @@ function fazerReOcorr(){
 		regLocal: $("#reOcorrLocal").val(),
 		regDescricaoS: $("#reOcorrDesc").val(),
 		regSetor: "Ouvidoria",
-		regStatus: "False"
+		regStatus: "Não Resolvido"
 	};
 	updates['/Ocorrencias-Refeitas/' + postKey] = postData;
 	firebase.database().ref().update(updates);
 	//Após salvar os dados no Banco o formulario é escondido
 	window.alert("Sua ocorrência foi refeita!");
-	cancelarReOcorr();
+	
+	$("#reOcorrReclamacao").val("");
+	$("#reOcorrEmail").val("");
+	$("#reOcorrLocal").val("");
+	$("#reOcorrDesc").val("");
 }
---
+++ public/sw.js
@@ -0,0 +1,100 @@
+var CACHE_STATIC_NAME = 'static-v15';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+  '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/promisse.min.js',
+    '/js/fetch.min.js',
+    '/js/meu-script.js',
+    '/js/visual-script.js',
+    '/js/bootstrap.min.js',
+    '/css/bootstrap.min.css',
+    '/css/meu-css.css',
+    '/imagens/logo/logo-01.jpg',
+    '/imagens/back-02.png',
+];
+
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(STATIC_FILES);
+      })
+  )
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length);
+  } else {
+    cachePath = string;
+  }
+  return array.indexOf(cachePath) > -1;
+}
+
+self.addEventListener('fetch', function (event) {
+  var url = 'https://httpbin.org/get';
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(function (cache) {
+          return fetch(event.request)
+            .then(function (res) {
+              cache.put(event.request, res.clone());
+              return res;
+            });
+        })
+    );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith(
+      caches.match(event.request)
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function (res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function (cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              })
+              .catch(function (err) {
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                      return cache.match('/offline.html');
+                    }
+                  });
+              });
+          }
+        })
+    );
+  }
+});
